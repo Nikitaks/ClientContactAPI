8795fa0297f7480f487d18c78855922d0b06e866
Nikitaks <n.aksenov2015@yandex.ru>
2023-12-01 09:16:25 +0300

    simplified description

--- src/main/java/com/ClientContactAPI/controller/ClientController.java
+++ src/main/java/com/ClientContactAPI/controller/ClientController.java
@@ -9,11 +9,6 @@ import com.ClientContactAPI.exception.InvalidDataException;
 import com.ClientContactAPI.mapper.EntityDTOmapper;
 import com.ClientContactAPI.service.ClientService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,42 +28,12 @@ public class ClientController {
     private EntityDTOmapper mapper;
 
     @Operation(summary = "Получение списка клиентов")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Список клиентов",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    array = @ArraySchema(schema = @Schema(implementation = ClientDTO.class)))
-                    })
-    })
     @GetMapping("/clients")
     public List<ClientDTO> getListClientsDTO() {
         return mapper.clientListToClientDTOList(clientService.findAllClients());
     }
 
     @Operation(summary = "Добавление нового клиента")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "Имя клиента")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "Сообщение об успешном добавлении",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Message.class))
-                    }),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Ошибка проверки входных данных",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Message.class))
-                    })
-    })
     @PostMapping(path = "/clients")
     public ResponseEntity<Message> addClient(@RequestBody ClientDTO clientDTO, HttpServletRequest request) {
         if ((clientDTO.getName() == null) || ("".equals(clientDTO.getName()))) {
@@ -80,24 +45,6 @@ public class ClientController {
     }
 
     @Operation(summary = "Получение информации по заданному клиенту (по id)")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Объект со всеми данными по клиенту",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = ClientContactsDTO.class))
-                    }),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Сообщение с причиной невозможности получения информации",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = Message.class))
-                    })
-    })
     @GetMapping("/clients/{clientId}")
     public ResponseEntity<ClientContactsDTO> getInfoClientId(@PathVariable Long clientId) throws InvalidDataException {
         if (clientId == null) {
--- src/main/java/com/ClientContactAPI/controller/ContactController.java
+++ src/main/java/com/ClientContactAPI/controller/ContactController.java
@@ -10,7 +10,6 @@ import com.ClientContactAPI.service.ClientService;
 import com.ClientContactAPI.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,11 +24,6 @@ import com.ClientContactAPI.entity.ContactType;
 import com.ClientContactAPI.exception.InvalidDataException;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 public class ContactController {
@@ -42,27 +36,6 @@ public class ContactController {
 	private EntityDTOmapper mapper;
 
 	@Operation(summary = "Добавление нового контакта клиента")
-	@io.swagger.v3.oas.annotations.parameters.RequestBody(
-			description = "Объект, содержащий Id клиента, тип контакта(EMAIL или TELEPHONE) "
-					+ "и сам контакт")
-	@ApiResponses(value = {
-			@ApiResponse(
-					responseCode = "201",
-					description = "Сообщение об успешном добавлении",
-					content = {
-							@Content(
-									mediaType = "application/json",
-									schema = @Schema(implementation = Message.class))
-					}),
-			@ApiResponse(
-					responseCode = "400",
-					description = "Причина невозможности добавления",
-					content = {
-							@Content(
-									mediaType = "application/json",
-									schema = @Schema(implementation = Message.class))
-					})
-	})
 	@PostMapping("/contacts")
 	public ResponseEntity<Message> addContact(@RequestBody ClientContactDTO contactsDTO) {
 		Message message = checkIfInvalidData(contactsDTO);
@@ -75,24 +48,6 @@ public class ContactController {
 
 
 	@Operation(summary = "Получение списка всех контактов заданного клиента или контактов заданного типа")
-	@ApiResponses(value = {
-    	@ApiResponse(
-           	responseCode = "200",
-           	description = "Список контактов",
-           	content = {
-               	@Content(
-                   	mediaType = "application/json",
-                   	array = @ArraySchema(schema = @Schema(implementation = ContactDTO.class)))
-            }),
-    	@ApiResponse(
-        	responseCode = "400",
-        	description = "Сообщение с причиной невозможности получения списка",
-        	content = {
-            	@Content(
-                	mediaType = "application/json",
-                	schema = @Schema(implementation = Message.class))
-        })
-    })
 	@GetMapping(path = "/contacts/{clientId}")
 	public ResponseEntity<List<ContactDTO>>
 		getListContactType(@PathVariable Long clientId,
